ca25df6b05c85efda04887936b96c75326305e7e
Jonathan <gustafsson.jonathan@yahoo.se>
2020-01-24 12:53:22 +0100

    loops

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -26,3 +26,44 @@ b2.innerHTML = burger2.getName();
 
 b3 = document.getElementById("b3");
 b3.innerHTML = burger3.getName();
+
+
+
+// -------------------------------- Loops --------------------------------------
+// Did not get for "burger in menu", look it up
+
+function menuItem(n, k, p){
+    this.name = n;
+    this.kCal = k;
+    this.picture = p;
+    this.getName = function(){
+        return this.name;
+    }
+    this.getkCal = function(){
+        return this.kCal;
+    };  
+}
+
+let burger1 = new menuItem("american dream", 1000, "");
+let burger2 = new menuItem("bacon attack", 1500, "");
+let burger3 = new menuItem("meatlovers nightmare", 700, "");
+
+
+
+burgers = [burger1, burger2, burger3];
+
+let menu = [{name: "american dream", kCal: "1000"}, {name: "bacon attack", kCal: "1000"} ,{name: "meatlovers nightmare", kCal: "500"}];
+
+console.log(menu[0].name);
+
+let myElement = document.getElementById("myID");
+
+let i = 0;
+for(i; i < menu.length; i++)
+{  
+	let listItem = document.createElement("li");
+	let listValue = document.createTextNode(menu[i].name);
+  listItem.appendChild(listValue);
+  myElement.appendChild(listItem);   
+}
+
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -3,15 +3,14 @@
 const vm = new Vue({
     el: '#myID',
     data: {
-      /* Then you can copy it to the Vue object like this */
       h: "Burgers",
-      b1:{name: "american dream", kCal: "1000"},
-      b2:{name: "bacon attack", kCal: "1000"},
-      b3:{name: "meatlovers nightmare", kCal: "500"}, 
+      menu: [{name: "american dream", kCal: "1000"}, {name: "bacon attack", kCal: "1000"}, 
+            {name: "meatlovers nightmare", kCal: "500"}],
     },
+    
     methods: {
       printString: function () {
-              
+              console.log("Hello world");
     }
     }
   })
